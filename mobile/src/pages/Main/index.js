2d7f93d8f4921e9040cbfce72432d7cb859bd693
Carlos Sudsilowsky <sudsilowsky@gmail.com>
2020-05-13 01:12:22 -0300

    Mobile template

---
+++ mobile/src/pages/Main/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+
+// import { Container } from './styles';
+
+const Main = () => {
+  return <View style={{backgroundColor: '#333', height: '100%'}} />;
+};
+
+export default Main;
